8bf8f1c4d8b607b5386d87e457d929a8070de95b
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-04-16 13:09:58 +0700

    assignment 1

---
+++ Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("nhap vao mot so\n");
+            number = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
