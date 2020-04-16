7071188aca8c395579d0a2344587125baee80da0
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-04-16 13:08:27 +0700

    make it better

--- Exercise 1/Program.cs
+++ Exercise 1/Program.cs
@@ -3,31 +3,31 @@
 namespace Exercise_1
 {
     internal class Program
-    {
-        public static void Main(string[] args)
         {
-            int number;
-            Console.WriteLine("nhap vao mot so\n");
-            number = Convert.ToInt32(Console.ReadLine());
-            int first_fibo = 0, second_fibo = 1, next_fibo, i;
-            for (i = 0; first_fibo+second_fibo < number; i++)
+            public static void Main(string[] args)
             {
+                int number;
+                Console.WriteLine("nhap vao mot so\n");
+                number = Convert.ToInt32(Console.ReadLine());
+                int first_fibo = 0, second_fibo = 1, next_fibo, i;
+                for (i = 0; first_fibo+second_fibo < number; i++)
+                {
                  
-                if (i <= 1)
-                {
-                    next_fibo = i;
-                }
-                else
-                {
-                    next_fibo = first_fibo + second_fibo;
-                    first_fibo = second_fibo;
-                    second_fibo = next_fibo;
-                }
+                    if (i <= 1)
+                    {
+                        next_fibo = i;
+                    }
+                    else
+                    {
+                        next_fibo = first_fibo + second_fibo;
+                        first_fibo = second_fibo;
+                        second_fibo = next_fibo;
+                    }
                 
-            }
-            Console.WriteLine("the Fibonacci number near "+number+ " is: ");
-            Console.WriteLine(second_fibo);
+                }
+                Console.WriteLine("the Fibonacci number near "+number+ " is: ");
+                Console.WriteLine(second_fibo);
             
+            }
         }
-    }
 }
