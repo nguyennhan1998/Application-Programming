53f0c93d3746695f4ac7307d896e821eef4f0eb9
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-04-16 12:45:30 +0700

    my first commit

---
+++ Application Programming/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Application_Programming
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Exercise 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
