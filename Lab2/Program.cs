a8c06bafe9580dff02644690fc52c236bc6651a9
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-04-19 12:41:46 +0700

    make it better

---
+++ Lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Xin chao nhan");
+            Product p1= new Product(1,"phone",56,1,"http://somethingJustLikeThis.jpg","red");
+            Console.WriteLine(p1.GetInformation());
+            p1.Status();
+            p1.GalleryAdd();
+            p1.GalleryAdd();
+            p1.GalleryAdd();
+            p1.showGallery();
+        }
+    }
+}
